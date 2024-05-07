cc5a04e058f280ea5dd4e46923e860303a32f4e8
Mariana Gonzaga Ramires <marianagzramires@gmail.com>
2024-05-07 19:03:49 -0400

    implementada toda tarefa 1

--- trabalho.c
+++ trabalho.c
@@ -32,19 +32,21 @@ char *get_key(void *reg)
     return (*((tmunicipio *)reg)).codigo_ibge;
 }
 
-void aloca_municipio(tmunicipio **municipio, char *codigo_ibge, char *nome, float latitude, float longitude,
-                     int capital, int codigo_uf, int siafi_id, int ddd, char *fuso_horario)
+void *aloca_municipio(char *codigo_ibge, char *nome, float latitude, float longitude,
+                      int capital, int codigo_uf, int siafi_id, int ddd, char *fuso_horario)
 {
+    tmunicipio *municipio = malloc(sizeof(tmunicipio));
+    strcpy(municipio->codigo_ibge, codigo_ibge);
+    strcpy(municipio->nome, nome);
+    municipio->latitude = latitude;
+    municipio->longitude = longitude;
+    municipio->capital = capital;
+    municipio->codigo_uf = codigo_uf;
+    municipio->siafi_id = siafi_id;
+    municipio->ddd = ddd;
+    strcpy(municipio->fuso_horario, fuso_horario);
 
-    strcpy((*municipio)->codigo_ibge, codigo_ibge);
-    strcpy((*municipio)->nome, nome);
-    (*municipio)->latitude = latitude;
-    (*municipio)->longitude = longitude;
-    (*municipio)->capital = capital;
-    (*municipio)->codigo_uf = codigo_uf;
-    (*municipio)->siafi_id = siafi_id;
-    (*municipio)->ddd = ddd;
-    strcpy((*municipio)->fuso_horario, fuso_horario);
+    return municipio;
 }
 
 char *trata(char *string)
@@ -89,7 +91,7 @@ uint32_t hashf(const char *str, uint32_t h)
 uint32_t hash_duplo(uint32_t h, int max, int i)
 {
     int hash = h + (i * ((h % (max - 1)) + 1));
-    return hash;
+    return abs(hash % max);
 }
 
 int hash_insere(thash *h, void *bucket)
@@ -118,15 +120,14 @@ int hash_insere(thash *h, void *bucket)
     return EXIT_SUCCESS;
 }
 
-int hash_constroi(thash *h)
+int hash_constroi(thash *h, int nbuckets, char *(*get_key)(void *))
 {
-    // 11003 por ser quase o dobro da qtnde de elementos e ser numero primo
-    h->table = calloc(sizeof(void *), 11003);
+    h->table = calloc(sizeof(void *), nbuckets);
     if (h->table == NULL)
     {
         return EXIT_FAILURE;
     }
-    h->max = 11003;
+    h->max = nbuckets;
     h->size = 0;
     h->get_key = get_key;
     h->deleted = (uintptr_t) & (h->size);
@@ -171,10 +172,9 @@ void municipio_printa(tmunicipio municipio)
 int main()
 {
     thash h;
-    hash_constroi(&h);
+    hash_constroi(&h, 11003, get_key);
     char c[500];
     char sem_aspas[500];
-    long int cont = 0;
     char codigo_ibge[10];
     char nome[40];
     float latitude;
@@ -186,67 +186,98 @@ int main()
     char fuso_horario[40];
     FILE *arq;
     arq = fopen("municipios.json", "r");
+    char busca[8];
+    int condicao = -1;
 
     fgets(c, 100, arq); // lixo
-                        // while (!feof(arq))
-    //{
-    fgets(c, 100, arq); // lixo
-
-    // codigo_ibge
-    fgets(c, 500, arq);
-    strcpy(codigo_ibge, trata(c));
-
-    // nome
-    fgets(c, 500, arq);
-    trata_aspas(c, sem_aspas);
-    strcpy(nome, sem_aspas);
-
-    // latitude
-    fgets(c, 500, arq);
-    latitude = atof(trata(c));
-
-    // longitude
-    fgets(c, 500, arq);
-    longitude = atof(trata(c));
-
-    // capital
-    fgets(c, 500, arq);
-    capital = atoi(trata(c));
-
-    // codigo_uf
-    fgets(c, 500, arq);
-    codigo_uf = atoi(trata(c));
-
-    // siafi_id
-    fgets(c, 500, arq);
-    siafi_id = atoi(trata(c));
-
-    // ddd
-    fgets(c, 500, arq);
-    ddd = atoi(trata(c));
-
-    // fuso horario
-    fgets(c, 500, arq);
-    trata_aspas(c, sem_aspas);
-    strcpy(fuso_horario, sem_aspas);
-
-    fgets(c, 100, arq); // lixo
-    //}
-
-    tmunicipio *temp = malloc(sizeof(tmunicipio));
-    aloca_municipio(&temp, codigo_ibge, nome, latitude, longitude, capital,
-                    codigo_uf, siafi_id, ddd, fuso_horario);
-
-    assert(hash_insere(&h, temp) == EXIT_SUCCESS);
-
-    tmunicipio *search = hash_busca(h, "5200050");
-    if (search != NULL)
+    while (!feof(arq))
     {
-        municipio_printa(*search);
+        fgets(c, 100, arq); // lixo
+
+        if (strchr(c, ']') != NULL)
+            break;
+
+        // codigo_ibge
+        fgets(c, 500, arq);
+        strcpy(codigo_ibge, trata(c));
+
+        // nome
+        fgets(c, 500, arq);
+        trata_aspas(c, sem_aspas);
+        strcpy(nome, sem_aspas);
+
+        // latitude
+        fgets(c, 500, arq);
+        latitude = atof(trata(c));
+
+        // longitude
+        fgets(c, 500, arq);
+        longitude = atof(trata(c));
+
+        // capital
+        fgets(c, 500, arq);
+        capital = atoi(trata(c));
+
+        // codigo_uf
+        fgets(c, 500, arq);
+        codigo_uf = atoi(trata(c));
+
+        // siafi_id
+        fgets(c, 500, arq);
+        siafi_id = atoi(trata(c));
+
+        // ddd
+        fgets(c, 500, arq);
+        ddd = atoi(trata(c));
+
+        // fuso horario
+        fgets(c, 500, arq);
+        trata_aspas(c, sem_aspas);
+        strcpy(fuso_horario, sem_aspas);
+
+        fgets(c, 100, arq); // lixo
+
+        hash_insere(&h, aloca_municipio(codigo_ibge, nome, latitude, longitude, capital,
+                                        codigo_uf, siafi_id, ddd, fuso_horario));
     }
-    else
+
+    while (condicao != 0)
     {
-        printf("Não encontrado.");
+        printf("-----\n");
+        printf("1 - Consulte uma cidade pelo seu código IBGE\n");
+        printf("2 - Consulte os N vizinhos mais próximos de uma cidade pelo seu código IBGE\n");
+        printf("3 - Consulte os N vizinhos mais próximos de uma cidade pelo seu nome \n");
+        printf("0 - Sair\n");
+        scanf("%d", &condicao);
+
+        switch (condicao)
+        {
+        case 1:
+            printf("Digite o código IBGE da cidade: ");
+            scanf("%s", busca);
+
+            tmunicipio *search = hash_busca(h, busca);
+            if (search != NULL)
+            {
+                municipio_printa(*search);
+            }
+            else
+            {
+                printf("Cidade não encontrada.");
+            }
+            break;
+        case 2:
+            printf("TO-DO");
+            break;
+        case 3:
+            printf("TO-DO");
+            break;
+        case 0:
+            break;
+        default:
+            printf("Essa não é uma opção válida, digite novamente");
+            break;
+        }
     }
 
     fclose(arq);
